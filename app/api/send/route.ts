48f2b90c11c7ce0637ae7a6907c94943e26e2b45
Zeno Rocha <zno.rocha@gmail.com>
2023-06-16 13:25:18 -0700

    Update handler from GET to POST

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -4,7 +4,7 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function GET() {
+export async function POST() {
   try {
     const data = await resend.emails.send({
       from: 'onboarding@resend.dev',
