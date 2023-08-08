033ea34d6749b71ef11cfdff8325f0993bba987b
Jayan Ratna <jayanratna@gmail.com>
2023-08-09 03:14:54 +1200

    Update `from` and `to` parameter usage (#3)

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -7,8 +7,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST() {
   try {
     const data = await resend.emails.send({
-      from: 'onboarding@resend.dev',
-      to: 'delivered@resend.dev',
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['delivered@resend.dev'],
       subject: "Hello world",
       react: EmailTemplate({ firstName: "John" }),
     });
