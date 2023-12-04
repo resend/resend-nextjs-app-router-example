b7e7b5e478157164d9de7a005b5de719f30a0936
Zeno Rocha <zno.rocha@gmail.com>
2023-12-04 07:10:05 -0300

    feat: Replace NextResponse with native Response

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,5 +1,4 @@
 import { EmailTemplate } from '../../../components/email-template';
-import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 import * as React from 'react';
 
@@ -8,18 +7,18 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST() {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Acme <onboarding@resends.dev>',
+      from: 'Acme <onboarding@resend.dev>',
       to: ['delivered@resend.dev'],
       subject: "Hello world",
       react: EmailTemplate({ firstName: "John" }) as React.ReactElement,
     });
 
     if (error) {
-      return NextResponse.json({ error });
+      return Response.json({ error });
     }
 
-    return NextResponse.json({ data });
+    return Response.json({ data });
   } catch (error) {
-    return NextResponse.json({ error });
+    return Response.json({ error });
   }
 }
